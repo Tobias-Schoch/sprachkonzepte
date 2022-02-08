e40cafd502717bdd612da04532f17bb6addf1d60
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2022-02-08 12:07:39 +0100

    Added static sematic tests

---
+++ sprachkonzepte-aufgabe4/src/Aufgabe4a.java
@@ -0,0 +1,36 @@
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Aufgabe4a {
+    private Aufgabe4a() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        soccerLexer lexer = new soccerLexer(CharStreams.fromStream(System.in));
+        soccerParser parser = new soccerParser(new CommonTokenStream(lexer));
+        ParseTree tree = parser.start();
+        if (parser.getNumberOfSyntaxErrors() > 0) {
+            System.err.printf("%d error(s) detected%n", parser.getNumberOfSyntaxErrors());
+            System.exit(1);
+        }
+
+        Matchup ast = new soccerBuilder().build(tree);
+
+        if(soccerBuilder.staticSemantic()) {
+            for (int i = ast.contentList.size() - 1; i >= 0; i--) {
+                if (i % 4 == 3 && i != ast.contentList.size() - 1) {
+                    System.out.print("\n");
+                } else if (i % 4 == 1) {
+                    System.out.print(" : ");
+                } else if (i != ast.contentList.size() - 1) {
+                    System.out.print(" ");
+                }
+
+                System.out.print(ast.contentList.get(i).toString(ast.contentList.get(i)));
+            }
+        } else {
+            System.out.print("Es können keine gleichen Teams gegen einander spielen");
+        }
+    }
+}
--- sprachkonzepte-aufgabe4/src/soccerBuilder.java
+++ sprachkonzepte-aufgabe4/src/soccerBuilder.java
@@ -1,13 +1,16 @@
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.lang.reflect.Array;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 import java.util.Stack;
 
 public final class soccerBuilder extends soccerParserBaseListener {
     private Matchup matchup;
     private Stack<Content> game = new Stack<Content>();
-    private Stack<Club> club_stack = new Stack<Club>();
+    private static Content[] gameCheck = new Content[6];
     private LinkedList<Content> test = new LinkedList<>();
 
     public Matchup build(ParseTree tree) {
@@ -15,16 +18,24 @@ public final class soccerBuilder extends soccerParserBaseListener {
         return matchup;
     }
 
+    public static boolean staticSemantic() {
+        if (gameCheck[0].toString(gameCheck[0]).equals(gameCheck[4].toString(gameCheck[4]))) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void exitGame(soccerParser.GameContext ctx) {
         for (int i = 0; i < ctx.getChildCount(); i++) {
             if (!ctx.getChild(i).getText().contains(":")) {
                 this.game.push(new Content(ctx.getChild(i).getText()));
+                gameCheck[i] = (new Content(ctx.getChild(i).getText()));
             }
         }
 
         int i = 0;
-        while( game.size() > 0) {
+        while(game.size() > 0) {
             test.add(i++, this.game.pop());
         }
         this.matchup = new Matchup(test);
